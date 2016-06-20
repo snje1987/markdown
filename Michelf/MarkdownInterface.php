fb30a6c867504571a0a2186eebd3f84d91ce7b9a
Yang Ming <yangming0116@163.com>
2016-06-20 22:14:23 +0800

    重新格式化

--- Michelf/MarkdownInterface.php
+++ Michelf/MarkdownInterface.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Markdown  -  A text-to-HTML conversion tool for web writers
  *
@@ -11,28 +12,29 @@
 namespace Michelf;
 
 /**
- * Markdown Parser Interface 
+ * Markdown Parser Interface
  */
 interface MarkdownInterface {
-	/**
-	 * Initialize the parser and return the result of its transform method.
-	 * This will work fine for derived classes too.
-	 *
-	 * @api
-	 *
-	 * @param  string $text
-	 * @return string
-	 */
-	public static function defaultTransform($text);
 
-	/**
-	 * Main function. Performs some preprocessing on the input text
-	 * and pass it through the document gamut.
-	 *
-	 * @api
-	 *
-	 * @param  string $text
-	 * @return string
-	 */
-	public function transform($text);
+    /**
+     * Initialize the parser and return the result of its transform method.
+     * This will work fine for derived classes too.
+     *
+     * @api
+     *
+     * @param  string $text
+     * @return string
+     */
+    public static function defaultTransform($text);
+
+    /**
+     * Main function. Performs some preprocessing on the input text
+     * and pass it through the document gamut.
+     *
+     * @api
+     *
+     * @param  string $text
+     * @return string
+     */
+    public function transform($text);
 }
